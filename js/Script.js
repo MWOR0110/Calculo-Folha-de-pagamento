bbcc56161c841cc993dff62a7d2c98d6a7263b29
MWOR0110 <oliveiramisael20@gmail.con>
2023-02-13 22:56:40 -0300

    1302

--- js/Script.js
+++ js/Script.js
@@ -1,39 +1,37 @@
 /*https://calculosonline.netlify.app/pages/javascript*/
 
+var salario =8000// Number(document.getElementById('salario').value)
+var dependentes = Number(document.getElementById("input-dependentes").value)
+var vale = Boolean(document.getElementById("checkbox").value)
 
+var botao = document.getElementById('botao1')
+botao.addEventListener('click',calcular)
 
-
-var salario = document.getElementById("input-salario").value
-
-var dependentes = document.getElementById("input-dependentes").value
-
-var vale = document.getElementById("checkbox").value
-
-var calc = document.getElementById("botao").value
-
-var se = document.getElementById("checkbox")
-
-
+console.log(salario)
 function calcular(){
 
+    
+    var irrf, inss, vt, resultadofinal
+
     switch(true) {
-        case (salario <= 1903.98):
-            var irrf  = (salario - 0)
+        case salario <= 1903.98:
+            irrf  = 0
             break;
-        case (salario >= 1903,99 && salario <= 2826.65):
-            var irrf  = (salario / 100) * 7.5;
+        case salario <= 2826.65:
+            irrf  = salario  * 0.075;
             break;
-        case (salario >= 2826.66 && salario <= 3751.06 ):
-            var irrf  = (salario / 100) * 15;
+        case salario <= 3751.06 :
+            irrf  = salario * 0.15;
             break;
-        case (salario >= 3751.07 && salario <= 4664.68 ):
-                var irrf  = (salario / 100) * 22.5;
+        case salario <= 4664.68 :
+            irrf  = salario * 0.225;
             break;
         default:
-                var irrf  = (salario / 100) * 27.5;
+            irrf  = salario * 0.275;
             break;
 
     }
+    
     /*IRRF até 1.903,98 isento
     1.903,99 a 2.826,65 é 7,5%
     2.826,66 a 3.751,06 é 15%
@@ -43,34 +41,36 @@ function calcular(){
 
 
     switch(true) {
-        case (salario <= 1302):
-            var inss = (salario / 100) * 7.5;
+        case salario <= 1302:
+            inss = salario * 0.075;
             break;
-        case (salario >= 1302.01 && salario <= 2571.29):
-            var inss = (salario / 100) * 9;
+        case salario > salario <= 2571.29:
+            inss = salario * 0.9;
             break;
-        case (salario > 2571.30 && salario < 3856.94 ):
-            var inss = (salario / 100) * 12;
+        case salario = salario < 3856.94 :
+            inss = salario * 0.12;
             break;
-        case (salario > 3856.95 && salario <= 7507.49):
-            var inss = (salario / 100) * 14;
+        case salario = salario <= 7507.49:
+            inss = salario * 0.14;
             break;
         default:
-            var inss = (7507.49 / 100) * 14;
+            inss = 7507.49 * 0.14;
             break;
     }
+    
 
-
-    if(checkbox){
-        var vt = (salario/100) * 6;
+    if(vale){
+        vt = salario* 0.6;
     }else{
-        var vt = (salario - salario);
+        vt = 0;
     }
+   
 
+    
+    resultadofinal = salario
 
-    var resultadofinal = salario - (inss+ irrf + vt)
-
-
+    document.getElementById('salbruto').innerText = resultadofinal
+    console.log(salario)
     /*
     salario < 1.302,00  -  7,5%
     salario > 1.302,01 < 2.571,29 - 9%
@@ -79,15 +79,15 @@ function calcular(){
     salario < 7.507,49 - 14%      com o valor do maximo
     */
 
-    var p = window.document.getElementById("resultado")
+    //var p = window.document.getElementById("resultado")
 
-    document.write(p.innerText("RESULTADO: " + resultadofinal))
+   // document.write(p.innerText("RESULTADO: " + resultadofinal))
+
+   
 
 
-
-    document.body.main.caixa2.salarioinicio.value(salario)
+    
 }
 
 
-var salario = 1000
-document.write(salario)
+
